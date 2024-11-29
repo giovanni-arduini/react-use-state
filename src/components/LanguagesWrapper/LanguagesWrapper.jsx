7863534400b2235093972e86d3188c1fa70de17b
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-29 16:37:13 +0100

    ADD useState

--- src/components/LanguagesWrapper/LanguagesWrapper.jsx
+++ src/components/LanguagesWrapper/LanguagesWrapper.jsx
@@ -1,14 +1,22 @@
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import languages from "../../languages";
+import { useState } from "react";
 
 function LanguagesWrapper() {
+  const [selectedLanguageId, setSelectedLanguageId] = useState(1);
+
   return (
     <section className="main">
       <ul>
-        {languages.map((language) => (
-          <li key={language.id}>
-            <Button label={language.title} />
+        {languages.map(({ id, title }) => (
+          <li key={id}>
+            <Button
+              onClick={() => {
+                setSelectedLanguageId;
+              }}
+              label={title}
+            />
           </li>
         ))}
       </ul>
